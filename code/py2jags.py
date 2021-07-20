121f51557090cf467c995d9716603362a392709e
jtjanecek <janecektyler@gmail.com>
2021-07-20 11:41:34 -0600

    Add bind argument

--- code/py2jags.py
+++ code/py2jags.py
@@ -6,6 +6,7 @@ parser.add_argument('--name', help='Model name', required=True)
 parser.add_argument('--conditions', help='The conditions csv', required=True)
 parser.add_argument('--rts', help='The reaction time and response csv', required=True)
 parser.add_argument('--groups', help='The group csv', required=True)
+parser.add_argument('--bind')
 
 cli_args = parser.parse_args()
 print(cli_args)
